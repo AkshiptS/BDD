be338b2652aa84234149d4f1a12b55bd0c691483
AkshiptS <aakshiptsharma1@gmail.com>
2025-06-29 16:13:12 +0530

    this is my first commit for bdd framework

---
+++ src/test/java/steps/loginSteps.java
@@ -0,0 +1,19 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginSteps {
+    @Given("User is on the login page")
+    public void userIsOnTheLoginPage() {
+    }
+
+    @When("User enters valid username and password")
+    public void userEntersValidUsernameAndPassword() {
+    }
+
+    @Then("User should be redirected to the home page")
+    public void userShouldBeRedirectedToTheHomePage() {
+    }
+}
